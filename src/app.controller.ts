51fe5f063e5155073de7ec57efb315f87129253f
Jan.Hrivna <Jan.Hrivna@adastragrp.com>
2022-04-24 23:09:18 +0200

    deleting hbs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppDataSource } from './db/app.datasource';
-import { Analyzy } from './db/entities/analyzy';
 import { Vzorky } from './db/entities/vzorky';
 
 @Controller()
@@ -12,8 +11,8 @@ export class AppController {
   @Render('index')
   async root() {
     const vzorky = await AppDataSource.getRepository(Vzorky).find()
-    console.log('vzorky', vzorky.map(v => v.druh))
-    return { vzorky }
+    console.log('vzorkya', vzorky.map(v => v.druh))
+    return { vzorky } 
   }
 
 }
