adfd5471636c0b85bc220a31824571909d2f395c
Jan.Hrivna <Jan.Hrivna@adastragrp.com>
2022-04-24 19:00:11 +0200

    vypsání dat z DB

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,9 +11,9 @@ export class AppController {
   @Get()
   @Render('index')
   async root() {
-    const res = await AppDataSource.getRepository(Vzorky).find()
-    console.log('res', res)
-    return { message: 'Hello from the Handlebars :)' };
+    const vzorky = await AppDataSource.getRepository(Vzorky).find()
+    console.log('vzorky', vzorky.map(v => v.druh))
+    return { vzorky }
   }
 
 }
