c3514039cd28be67a64cb31566cd93884bd1c1f9
Jan.Hrivna <Jan.Hrivna@adastragrp.com>
2022-05-01 12:52:07 +0200

    monorepo init

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  @Render('index')
+  // @Render('index')
   async root() {
     const vzorky = await AppDataSource.getRepository(Vzorky).find()
     console.log('vzorkya', vzorky.map(v => v.druh))
