08337ca7fd9763eed136a0dee3aa0c93e712fd2d
Jan.Hrivna <Jan.Hrivna@adastragrp.com>
2022-05-03 23:27:06 +0200

    zprovoznění nx projektu

---
+++ apps/api/src/app/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AppDataSource } from './db/app.datasource';
+import { Vzorky } from './db/entities/vzorky';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  async root() {
+    const vzorky = await AppDataSource.getRepository(Vzorky).find()
+    console.log('vzorkya', vzorky.map(v => v.druh))
+    return { vzorky } 
+  }
+
+}
