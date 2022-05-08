8c009d5be7e53174b1cc7ceddbe50a8414c45b9d
Jan.Hrivna <Jan.Hrivna@adastragrp.com>
2022-05-08 19:44:43 +0200

    be - swagger

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -2,18 +2,14 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppDataSource } from './db/app.datasource';
 import { Vzorky } from './db/entities/vzorky';
-import { MyEnt } from '@impakt/model';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  async root() {
-    let x: MyEnt
-    const vzorky = await AppDataSource.getRepository(Vzorky).find()
-    console.log('vzorkya', vzorky.map(v => v.druh))
-    return { vzorky }
+  getVzorky() {
+    return AppDataSource.getRepository(Vzorky).find()
   }
 
 }
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,17 +1,23 @@
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  app.setGlobalPrefix('api');
+  const port = 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Impakt')
+    .setDescription('Impakt API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
 }
 
 bootstrap();
