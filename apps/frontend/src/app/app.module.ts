486a3e721a5688cfbc360434a7715c03ea22e5e0
Jan.Hrivna <Jan.Hrivna@adastragrp.com>
2022-05-16 00:38:10 +0200

    nový vzorek - form

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateVzorekModalComponent } from './components/create-vzorek-modal/create-vzorek-modal.component';
 import { OverviewComponent } from './components/overview/overview.component';
 
 @NgModule({
-  declarations: [AppComponent, OverviewComponent],
-  imports: [BrowserModule, HttpClientModule, NgbModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    OverviewComponent,
+    CreateVzorekModalComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    NgbModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/frontend/src/app/components/create-vzorek-modal/create-vzorek-modal.component.ts
+++ apps/frontend/src/app/components/create-vzorek-modal/create-vzorek-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { CreateVzorekFormEnum } from './create-vzorek-modal-form.enum';
 import { CreateVzorekModalService } from './create-vzorek-modal.service';
 
 @Component({
@@ -12,10 +13,16 @@ import { CreateVzorekModalService } from './create-vzorek-modal.service';
 export class CreateVzorekModalComponent implements OnInit {
 
   readonly form: FormGroup = this.createVzorekModalService.initForm()
+  readonly FormNames = CreateVzorekFormEnum
 
   constructor(
     public modal: NgbActiveModal,
     private createVzorekModalService: CreateVzorekModalService) {
+  }
+
+  onSave() {
+    this.modal.close()
+    console.log("form submit", this.form.getRawValue())
   }
 
   ngOnInit(): void { }
