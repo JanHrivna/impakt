4441f3ff66653bd73690361d9e85e2b0c95b4688
Jan.Hrivna <Jan.Hrivna@adastragrp.com>
2022-05-17 23:12:50 +0200

    měsíce v datapickeru česky

--- apps/frontend/src/app/components/create-vzorek-modal/create-vzorek-modal.component.ts
+++ apps/frontend/src/app/components/create-vzorek-modal/create-vzorek-modal.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { NgbActiveModal, NgbDateAdapter, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbDateAdapter, NgbDateParserFormatter, NgbDatepickerI18n } from '@ng-bootstrap/ng-bootstrap';
 import { CreateVzorekFormEnum } from './create-vzorek-modal-form.enum';
 import { CreateVzorekModalService } from './services/create-vzorek-modal.service';
-import { CustomDateParserFormatterService } from './services/custom-data-parser-formatter.service';
+import { CustomDateParserFormatterService } from './services/custom-date-parser-formatter.service';
 import { CustomDateAdapterService } from './services/custom-date-adapter.service';
+import { CustomDatepickerI18n, I18n } from './services/custom-datepicker-i18n.service';
 
 @Component({
   selector: 'frontend-create-vzorek-modal',
   templateUrl: './create-vzorek-modal.component.html',
   styleUrls: ['./create-vzorek-modal.component.scss'],
   providers: [CreateVzorekModalService,
+    I18n,
     { provide: NgbDateAdapter, useClass: CustomDateAdapterService },
-    { provide: NgbDateParserFormatter, useClass: CustomDateParserFormatterService }
+    { provide: NgbDateParserFormatter, useClass: CustomDateParserFormatterService },
+    { provide: NgbDatepickerI18n, useClass: CustomDatepickerI18n }
   ]
 })
 export class CreateVzorekModalComponent implements OnInit {
